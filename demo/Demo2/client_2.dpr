82433ecf034b1a4472618d9e1882e482f4d595db
Mihaela Mihaljevic Jakic <mihaela@token.hr>
2012-04-03 14:40:16 +0200

    changed renamed TXS_MsgRec

--- demo/Demo2/client_2.dpr
+++ demo/Demo2/client_2.dpr
@@ -30,8 +30,8 @@ var
   pxsContext      : Pointer;
   pxsClientSocket : Pointer;
 
-  xsServerMessage : TXS_Msg;
-  xsClientMessage : TXS_Msg;
+  xsServerMessage : TXS_MsgRec;
+  xsClientMessage : TXS_MsgRec;
 
   recBytes,
   sndBytes        : integer;
